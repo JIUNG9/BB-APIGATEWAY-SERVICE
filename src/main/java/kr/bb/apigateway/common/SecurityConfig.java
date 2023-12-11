be383dd771022586f03f3c14151cb6bdc800b130
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-11 15:50:39 +0900

    add: add the cofig

---
+++ src/main/java/kr/bb/apigateway/common/SecurityConfig.java
@@ -0,0 +1,20 @@
+package kr.bb.apigateway.common;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    http
+        .csrf(csrf -> csrf.disable());
+    return http.build();
+
+  }
+}

718db55ee49e2f42cbd85554d5ff16cf1b31ffd7
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-19 20:25:50 +0900

    refactor: refactor the jedis config to redis config

--- src/main/java/kr/bb/apigateway/common/config/RedisConfig.java
+++ src/main/java/kr/bb/apigateway/common/config/RedisConfig.java
@@ -3,8 +3,9 @@ package kr.bb.apigateway.common.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -22,20 +23,19 @@ public class RedisConfig {
 
 
   @Bean
-  JedisConnectionFactory jedisConnectionFactory() {
-    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(
-        host, port);
+  RedisConnectionFactory jedisConnectionFactory() {
+       RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
     redisStandaloneConfiguration.setHostName(host);
     redisStandaloneConfiguration.setPort(port);
     redisStandaloneConfiguration.setPassword(password);
-    return new JedisConnectionFactory(redisStandaloneConfiguration);
+    return new LettuceConnectionFactory(redisStandaloneConfiguration);
   }
 
   @Bean
   public RedisTemplate<String, Object> redisTemplate(
-      JedisConnectionFactory jedisConnectionFactory) {
+      RedisConnectionFactory redisConnectionFactory) {
     RedisTemplate<String, Object> template = new RedisTemplate<>();
-    template.setConnectionFactory(jedisConnectionFactory);
+    template.setConnectionFactory(redisConnectionFactory);
     template.setKeySerializer(new StringRedisSerializer());
     template.setValueSerializer(new StringRedisSerializer());
     template.setHashKeySerializer(new GenericToStringSerializer<>(String.class));
